26dfc99af1b3f02c188c08b59615c5effb2ec692
ItsaPlant <83190454+ItsaPlant@users.noreply.github.com>
2024-02-04 15:51:14 +0100

    Update contact_card.py

--- contact_card.py
+++ contact_card.py
@@ -1,51 +1,30 @@
 from faker import Faker
-from faker.providers import DynamicProvider
+from faker.providers import BaseProvider
 
 
-arch_professions_provider = DynamicProvider(
-    provider_name="architectural_profession",
-    elements=["arch", "MARCH", "Senior Architect",
-              "Intern Architect", "consultant", "za miskę ryzu"]
-)
+class ArchitecturalProfessionProvider(BaseProvider):
+    def architectural_profession(self):
+        return self.random_element(["arch", "MARCH", "Senior Architect", "Intern Architect", "consultant", "za miskę ryzu"])
+
 
 fake = Faker()
-fake.add_provider(arch_professions_provider)
+fake.add_provider(ArchitecturalProfessionProvider)
 
 
 class ContactCard:
     def __init__(self, name, surname, phone):
         self.name = name
         self.surname = surname
-        self._fullname_len = None
         self.phone = phone
-        
 
-    def __str__(self) -> str: #IDE mi tak wrzuciło, ale po co ta strzałka?
+    def __str__(self):
         return f'{self.name} {self.surname} -> {self.phone}'
-    
 
     def __repr__(self) -> str: #IDE mi tak wrzuciło, ale po co ta strzałka?
         return f'{self.name} {self.surname} -> {self.phone}'
-    
-
-    def __ge__(self, other, param): #jak rozwinąć taki pomysl?
-        return self.param >= other.param
-    
 
     def contact(self):
         return f'Kontaktuje się z: {self.name} {self.surname}, pod numerem: {self.phone}'
-    
-
-    @property
-    def label_length(self):
-        return self._fullname_len
-    
-
-    @label_length.setter
-    def label_length(self):
-        name_len = len(self.name)
-        surname_len = len(self.surname)
-        self._fullname_len = name_len + surname_len    
 
 
 class BusinessCard(ContactCard):
@@ -55,57 +34,37 @@ class BusinessCard(ContactCard):
         self.position = position
         self.phone_work = phone_work
 
-    
-    def __str__(self) -> str: #IDE mi tak wrzuciło, ale po co ta strzałka?
+    def __str__(self):
         return f'{self.name} {self.surname} -> {self.phone_work}'
     
-
     def __repr__(self) -> str: #IDE mi tak wrzuciło, ale po co ta strzałka?
         return f'{self.name} {self.surname} -> {self.phone_work}'
-    
 
     def contact(self):
         return f'Kontaktuje się z: {self.name} {self.surname}, pod numerem: {self.phone_work}'
 
 
 def generate():
-        name = fake.first_name()
-        surname = fake.last_name()
-        mail = fake.email()
-        phone = fake.phone_number()
-        company = fake.job()
-        position = fake.architectural_profession()
-        phone_work = fake.phone_number()
-        return (name, surname, mail, phone, company, position, phone_work)
+    name = fake.first_name()
+    surname = fake.last_name()
+    phone = fake.phone_number()
+    company = fake.job()
+    position = fake.architectural_profession()
+    phone_work = fake.phone_number()
+    return name, surname, phone, company, position, phone_work
 
 
-def create_contacts(type, number):
+def create_contacts(card_type, number):
     cards = []
-    name, surname, mail, phone, company, position, phone_work = generate()
-    if type == 'B':
-        for i in range(number):
-            b_card = BusinessCard(name, surname, phone)
+    for i in range(number):
+        name, surname, phone, company, position, phone_work = generate()
+        if card_type == 'B':
+            b_card = BusinessCard(company=company, position=position, phone_work=phone_work, name=name, surname=surname, phone=phone)
             cards.append(b_card)
-    elif type == 'C':
-        for i in range(number):
-            c_card = ContactCard(name, surname, mail, phone, company, position, phone_work)
+        elif card_type == 'C':
+            c_card = ContactCard(name=name, surname=surname, phone=phone)
             cards.append(c_card)
     return cards
 
-
 if __name__ == "__main__":
     print(create_contacts('B', 10))
-
-# class Obj:
-#     def __init__(self):
-#         self.var_1 = 1
-#         self.var_2 = 2
-
-
-# def return_obj_variable(obj, variable_name):
-#     variable_name = operator.attrgetter('var_1')
-#     return obj.variable_name
-
-# obj = Obj()
-
-# print(return_obj_variable(obj, 'var_1'))
